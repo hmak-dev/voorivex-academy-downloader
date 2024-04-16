9ae1d3aa917ab99fe56de29dba55f10469855a86
Hossein Maktoobian <8bitsight@gmail.com>
2024-04-16 20:37:40 +0330

    refactor: use recursive approach

--- src/index.js
+++ src/index.js
@@ -9,12 +9,113 @@ const log = require('./log');
 
 log.initLogs();
 
+async function processVideo(video, basePath) {
+	let videoFailCount = 0;
+
+	if (!log.logs.success.includes(video.key)) {
+		try {
+			message(`Removing Old Video`);
+			let removeFailCount = 0;
+			let removeOk = false;
+			while (!removeOk) {
+				await delay(1000);
+				const response = await api.remove(video.key);
+
+				if (response.data === true) {
+					removeOk = true;
+				} else {
+					removeFailCount++;
+
+					if (removeFailCount > 3) {
+						throw `Removing Old Video Failed 3 Times`;
+					}
+				}
+			}
+
+			message(`Generating New Video`);
+			let generateFailCount = 0;
+			let generateOk = false;
+			while (!generateOk) {
+				await delay(1000);
+				const response = await api.generate(video.key);
+
+				if (response.data === 'your action is added to queue') {
+					generateOk = true;
+				} else {
+					generateFailCount++;
+
+					if (generateFailCount > 3) {
+						throw `Generating New Video Failed 3 Times`;
+					}
+				}
+			}
+
+			message(`Getting Video URL`);
+			let urlFailCount = 0;
+			let url = null;
+			while (url === null) {
+				await delay(2000);
+				const response = await api.getActiveLink();
+
+				if (response.data.type === 'active' && response.data.videos.length > 0) {
+					url = response.data.videos[0].url;
+				} else {
+					urlFailCount++;
+
+					if (urlFailCount > 3) {
+						throw `Getting Video URL Failed 3 Times`;
+					}
+				}
+			}
+
+			const videoPath = path.resolve(basePath, video.title);
+			await downloadFile(url, videoPath, (progress) => {
+				message(`Downloading '${video.key.split('/').at(-1)}'   ::   ${numberToSize(progress.loaded)} of ${numberToSize(progress.total)}   ${(progress.progress * 100).toFixed(2)}%   ::   ${numberToSize(progress.rate)}/s   ::   ${numberToTime(progress.estimated)}`);
+			});
+
+			log.logSuccess(video.key);
+
+			message(`Downloaded '${video.key}'\n`);
+		} catch (error) {
+			videoFailCount++;
+
+			log.logError(error);
+			log.logFail(video.key);
+
+			message(`Failed to Download '${video.key}'\n`);
+
+			if (videoFailCount > 10) {
+				message(`Failed to Get 10 Videos. Exiting...\n`);
+
+				process.exit(1);
+			}
+
+			await delay(10000 * videoFailCount);
+		}
+	}
+}
+
+async function traverse(item, basePath) {
+	if (item.type === 'folder') {
+		const dir = path.resolve(basePath, item.title);
+
+		if (!fs.existsSync(dir)) {
+			fs.mkdirSync(dir);
+		}
+
+		for (let child of item.children) {
+			await traverse(child, dir);
+		}
+	} else if (item.type === 'file') {
+		await processVideo(item, basePath)
+	}
+}
+
 (async () => {
 	let failCount = 0;
 
 	try {
 		let dataFailCount = 0;
-		let videoFailCount = 0;
 
 		message(`Getting Video Catalog`);
 
@@ -40,94 +141,10 @@ log.initLogs();
 
 		message(`Video Catalog Downloaded\n`);
 
-		initDirectories(data);
+		const basePath = path.resolve(config.downloadDir);
 
-		loopCourse: for (const course of data) {
-			loopFolder: for (const folder of course.children) {
-				loopVideo: for (const video of folder.children) {
-					if (!log.logs.success.includes(video.key)) {
-						try {
-							message(`Removing Old Video`);
-							let removeFailCount = 0;
-							let removeOk = false;
-							loopRemove: while (!removeOk) {
-								await delay(1000);
-								const response = await api.remove(video.key);
-
-								if (response.data === true) {
-									removeOk = true;
-								} else {
-									removeFailCount++;
-
-									if (removeFailCount > 3) {
-										throw `Removing Old Video Failed 3 Times`;
-									}
-								}
-							}
-
-							message(`Generating New Video`);
-							let generateFailCount = 0;
-							let generateOk = false;
-							loopGenerate: while (!generateOk) {
-								await delay(1000);
-								const response = await api.generate(video.key);
-
-								if (response.data === 'your action is added to queue') {
-									generateOk = true;
-								} else {
-									generateFailCount++;
-
-									if (generateFailCount > 3) {
-										throw `Generating New Video Failed 3 Times`;
-									}
-								}
-							}
-
-							message(`Getting Video URL`);
-							let urlFailCount = 0;
-							let url = null;
-							loopUrl: while (url === null) {
-								await delay(2000);
-								const response = await api.getActiveLink();
-
-								if (response.data.type === 'active' && response.data.videos.length > 0) {
-									url = response.data.videos[0].url;
-								} else {
-									urlFailCount++;
-
-									if (urlFailCount > 3) {
-										throw `Getting Video URL Failed 3 Times`;
-									}
-								}
-							}
-
-							const videoPath = path.resolve(config.downloadDir, course.title, folder.title, video.title);
-							await downloadFile(url, videoPath, (progress) => {
-								message(`Downloading '${video.key}'   ::   Downloaded: ${numberToSize(progress.loaded)} of ${numberToSize(progress.total)}   ${(progress.progress * 100).toFixed(2)}%   ::   Rate: ${numberToSize(progress.rate)}/s   ::   Estimated: ${numberToTime(progress.estimated)}`);
-							});
-
-							log.logSuccess(video.key);
-
-							message(`Downloaded '${video.key}'\n`);
-						} catch (error) {
-							videoFailCount++;
-
-							log.logError(error);
-							log.logFail(video.key);
-
-							message(`Failed to Download '${video.key}'\n`);
-
-							if (videoFailCount > 10) {
-								message(`Failed to Get 10 Videos. Exiting...\n`);
-
-								process.exit(1);
-							}
-
-							await delay(10000 * videoFailCount);
-						}
-					}
-				}
-			}
+		for (let collection of data) {
+			await traverse(collection, basePath);
 		}
 	} catch (error) {
 		log.logError(error);
